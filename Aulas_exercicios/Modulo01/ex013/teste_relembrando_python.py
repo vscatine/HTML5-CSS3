0f96f6d053bcc90e3f7fb2bd70f94d7d8febc3b6
vscatine <vitor.scatine@gmail.com>
2022-08-25 18:31:07 -0300

    excluindo o teste de python

--- Aulas_exercicios/Modulo01/ex013/teste_relembrando_python.py
+++
@@ -1,7 +0,0 @@
-print("Hello World!")
-print("O Eike vai ver esse código mudando no GitHub")
-m = input("Você é um otário? ")
-if m =="S":
-    print("Olá, Otário!")
-else:
-    print("Na verdade você é um otário, sim!")
